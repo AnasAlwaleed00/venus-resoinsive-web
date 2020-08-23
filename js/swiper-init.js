badb88790a80b221cf7ccb2da3def7a90481caf2
Anas Alwaleed <anasalwaleed77@gmail.com>
2020-08-24 01:49:54 +0200

    -The website is now responsive

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -4,6 +4,34 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerGroup: 3,
   loop: true,
   loopFillGroupWithBlank: true,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 10
+    }
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -12,7 +40,10 @@ var swiper = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  
 });
+
+
 
 var swiper = new Swiper('.swiper-container-2', {
   slidesPerView: 2,
@@ -20,6 +51,28 @@ var swiper = new Swiper('.swiper-container-2', {
   slidesPerGroup: 2,
   loop: true,
   loopFillGroupWithBlank: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 10
+    },
+  },
+
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
